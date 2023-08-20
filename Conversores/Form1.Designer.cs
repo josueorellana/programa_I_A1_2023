80465ce19789e9cd4f399d68358f4cead5d92610
YaritzaEscobar <escobarjaqueline18@gmail.com>
2023-08-20 17:18:24 -0600

    TextBox

--- Conversores/Form1.Designer.cs
+++ Conversores/Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace Conversores
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -61,11 +62,19 @@ namespace Conversores
             this.label2.TabIndex = 2;
             this.label2.Text = "label2";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(234, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
@@ -81,6 +90,7 @@ namespace Conversores
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
