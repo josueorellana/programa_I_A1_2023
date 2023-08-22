ded451bec5aa289842286af5e3ff2030491f0cad
Wilbert José Girón Pérez <USSS039723@ugb.edu.sv>
2023-08-21 21:16:59 -0600

    ASH

--- Conversores/Form1.cs
+++ Conversores/Form1.cs
@@ -18,7 +18,8 @@ namespace Conversores
             new string[]{"Libras", "Miligramos", "Gramos", "Onzas", "Tonelada Corta", "Kilogramos", "Tonelada Métrica", "Stone", "Kilonewton" }, // Masa
             new string[]{"Litro", "Mililitro", "Metro Cubico", "Pie Cubico", "Pulgada Cubica", "Galón Imperial", "Galón de EE. UU.", "Metro Cúbico de Gas", "Pie Cúbico de Gas" }, // Volumen
             new string[]{"Metro", "Milimetro", "Centimetro", "Pulgada", "Pies", "Varas", "Yardas", "Kilometros", "Millas", "Nanometros" }, // Longitud
-            new string[]{"Microsegundo", "Milisegundo", "Segundo", "Minuto", "Hora", "Dia", "Semana", "Mes", "Año", "Decada",} // Tiempo
+            new string[]{"Bit", "Byte", "Kilobyte", "Megabyte", "Gigabyte", "Terabyte", "Petabyte", "Exabyte", "Zettabyte", "Yottabyte"}, // almacenamiento
+            new string[]{"Microsegundo", "Milisegundo", "Segundo", "Minuto", "Hora", "Dia", "Semana", "Mes", "Año", "Decada"}, // Tiempo
         };
 
         public Conversor()
@@ -48,7 +49,8 @@ namespace Conversores
             new double[]{ 1, 453592, 453.59, 16, 0.0005, 0.453592, 0.000453592, 0.0714286, 0.0044 }, // Masa 
             new double[]{ 1, 1000, 0.001, 0.0353147, 61.0237, 0.219969, 0.264172, 0.001, 28.3168 }, // Volumen 
             new double[]{ 1, 1000, 100, 39.3701, 3.28084, 1.1963081929167, 1.0936, 0.001, 0.000621371, 1000000000 }, // Longitud
-            new double[]{ 1, 0.002, 0.000001, 60000000, 3600000000, 86400000000, 604800000000, 2628000000000, 31536000000000, 315360000000000, } // Tiempo (Microsegundo como base)
+            new double[]{ 1, 0.125,0.0001220703125, 1.1920928955078e-7, 1.1641532182693e-10, 1.1368683772162e-13, 1.1102230246252e-16, 1.0842021724855e-19, 1.0587911840679e-22, 1.0339757656913e-25 }, // Almacenamiento
+            new double[]{ 1, 0.002, 0.000001, 60000000, 3600000000, 86400000000, 604800000000, 2628000000000, 31536000000000, 315360000000000 }, // Tiempo
         };
             respuesta = monedas[cboTipoConversor.SelectedIndex][a] / monedas[cboTipoConversor.SelectedIndex][de] * cantidad;
             lblRespuestaConversor.Text = "Respuesta: " + Math.Round(respuesta, 3);
@@ -64,6 +66,7 @@ namespace Conversores
 
         private void cboTipoConversor_TextChanged(object sender, EventArgs e)
         {
+
             cboDeConversor.Items.Clear();
             cboDeConversor.Items.AddRange(tiposConversores[cboTipoConversor.SelectedIndex]);
 
